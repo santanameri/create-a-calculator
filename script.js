9cb4e974a68ab00851af7055a791b96f6d3cd070
Merielly Santana <santana.meri10gmail.com>
2023-01-10 12:43:22 -0300

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let currentInput = document.querySelector('.currentInput');
 let answerScreen = document.querySelector('.answerScreen');
-let buttons = document.querySelector('button');
+let buttons = document.querySelectorAll('button');
 let erasebtn = document.querySelector('#erase');
 let clearbtn = document.querySelector('#clear');
 let evaluate = document.querySelector('#evaluate');
